485ff213900c80c157c4d58ed65857a9c375b7a9
angelo souza <angelogm2005@gmail.com>
2024-05-28 22:05:57 -0300

    progresso

--- src/components/layout/singIn/content.jsx
+++ src/components/layout/singIn/content.jsx
@@ -5,23 +5,32 @@ import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+// imagens 
 import Logo from '../../../assets/newWorld.png';
 import Globo from '../../../assets/login/GloboSmall.gif';
 import ArrowUp from '../../../assets/login/Arrow 1.jpg';
 import ArrowDown from '../../../assets/login/Arrow 4.jpg';
 import Persona1 from '../../../assets/login/Persona1.png';
 import Persona2 from '../../../assets/login/Persona2.png';
+import USA from '../../../assets/login/USAflag.png';
+import BR from '../../../assets/login/BRflag.png';
 
 const Content = () => {
   return (
     <>
 
-      <Container id='all'>
+      <Container id='all' >
+
+        {/* imagem da logo do site Hello World */}
 
         <Row className='d-flex justify-content-center logoLogin' xs={4} md={5} style={{ marginBottom: "40px", }}>
-          <Image className='logo' style={{ padding: '0px', minHeight: '200px' }} src={Logo} />
+          <figure>
+            <Image className='logo' style={{ padding: '0px', minHeight: '200px' }} src={Logo} />
+          </figure>
+
         </Row>
 
+        {/* texto de apresentação do site */}
 
         <Row id='middleText' >
           <h4>
@@ -29,34 +38,43 @@ const Content = () => {
           </h4>
         </Row>
 
-
+        {/* div de testemunho de personas  */}
 
         <Row style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
 
+          {/* persona 1 */}
+
           <Col>
-          
-            <figure>
+
+            <figure className='figurePersona'>
 
               <Image src={Persona1} style={{ margin: '0px' }} />
-              <figcaption>Minha jornada no HELLO WORLD tem sido uma verdadeira troca cultural! Conectar-me com amigos americanos me trouxe uma compreensão mais profunda da diversidade e riqueza do mundo. </figcaption>
+
+              <figcaption className='textPersona'>Minha jornada no HELLO WORLD tem sido uma verdadeira troca cultural!  </figcaption>
 
             </figure>
 
           </Col>
 
-          <Col>
+          {/* flechas de rotação */}
+
+          <Col xs lg="2">
 
             <div> <Image src={ArrowUp} className='arrowUp' /> </div>
 
             <div> <Image src={ArrowDown} /> </div>
 
           </Col>
+
+          {/* globo do mundo giff girando */}
 
           <Col>
             <Image src={Globo} />
           </Col>
 
-          <Col>
+          {/* flechas de rotação */}
+
+          <Col xs lg="2">
 
             <div> <Image src={ArrowUp} className='arrowUp' /> </div>
 
@@ -64,12 +82,36 @@ const Content = () => {
 
           </Col>
 
+          {/* persona 2 */}
+
           <Col>
 
-            <figure>
+            <figure className='figurePersona'>
 
               <Image src={Persona2} />
-              <figcaption>Minha jornada no HELLO WORLD tem sido uma verdadeira troca cultural! Conectar-me com amigos americanos me trouxe uma compreensão mais profunda da diversidade e riqueza do mundo. </figcaption>
+
+              <div className='personaFlagsDiv'>
+
+                
+
+
+                <figure className='figureFlag'>
+
+                  <Image src={BR}/>
+
+                </figure>
+
+                <figcaption>ENG → PT</figcaption>
+
+                <figure className='figureFlag'>
+
+                  <Image src={USA}/>
+
+                </figure> 
+
+              </div>
+
+              <figcaption className='textPersona'>Conectar-me com amigos americanos me trouxe uma compreensão do mundo. </figcaption>
 
             </figure>
 
